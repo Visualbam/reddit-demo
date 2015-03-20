9e69762e804716ebab3f673d6a4858de6a65d05b
Bruce McElroy <bruce.mcelroy@gmail.com>
2015-03-20 15:01:15 -0400

    Updated mark up to use old styles

--- scripts/compositionRoot.js
+++ scripts/compositionRoot.js
@@ -3,7 +3,7 @@
 (function (window, $, ko, homeController, ViewEngine) {
     "use strict";
 
-    var App = new Sammy('#content', function () {
+    var App = new Sammy('#main', function () {
         // Wire up controllers: Pass sammy into controller function
         window.homeController(this);
         ko.applyBindings(ViewEngine.mainVw);

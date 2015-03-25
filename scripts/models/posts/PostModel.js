55ca7fbc0e1d7417c394959a919746287087d083
Bruce McElroy <bruce.mcelroy@gmail.com>
2015-03-25 16:02:51 -0400

    show all images

--- scripts/models/posts/PostModel.js
+++ scripts/models/posts/PostModel.js
@@ -10,14 +10,10 @@ app.register({
         return function (data) {
             data = data || {};
 
-            var imageUrl = data.url.toString().replace('http://imgur.com/', 'https://i.imgur.com/');
+            var imageUrl = data.url.toString().replace('http://imgur.com/', 'https://i.imgur.com/').replace('gifv', 'gif');
 
             if (imageUrl.indexOf('.jpg') <= 0) {
                 imageUrl = imageUrl + '.jpg';
-            }
-
-            if (imageUrl.indexOf('.gifv') >= 0) {
-                imageUrl = 'http://www.pakistancardealers.com/img/no-image.jpg';
             }
 
             this.title = ko.observable(data.title || '');
